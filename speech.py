dca99af04985b76023d2fa81116022e16f281f75
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-04-27 03:20:51 -0400

    speech.py the program file has been updated

--- speech.py
+++ speech.py
@@ -1,18 +1,25 @@
 # this is a python code that takes a text file as input and converts it to speech
 # using the google text to speech api
-from gtts import gTTS
 import os
 
-# this is to install the vlc only for linux with apt
-os.system("sudo apt install vlc")
+# installing all the prerequisites
+os.system("pip install gtts")
+os.system("sudo apt install vlc -y")
+
+# importing the required packages
+from gtts import gTTS
 
 # read the text file
-file = open("LICENSE", "r")
+filename = input("Enter the filename with extension(should be inside the same folder) : ")
+file = open(filename, "r")
 text = file.read().replace("\n", " ")
 
 # convert the text to speech
+audio = input("Enter the name with which you want to save the audio file(without extension) : " )
 tts = gTTS(text=text, lang="en")
-tts.save("output.mp3")
+tts.save(f"{audio}.mp3")
+
+# closing the file
 file.close()
 
 # this is to play the audio file using cvlc
